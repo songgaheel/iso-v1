21caa64781a906b152f55fe4e3778f19ff562fba
songgaheel <songgaheel@gmail.com>
2020-08-28 05:41:01 +0700

    work eval create

--- controllers/work-eval-controller.js
+++ controllers/work-eval-controller.js
@@ -31,9 +31,98 @@ const workevalCreate3 = (req, res, next) => {
         }
     });
 }
-
+const workevalCreate4 = (req, res, next) => {
+    res.render("work-eval-create-4", {
+        data: {
+            pageName: "ความเสี่ยงในกิจกรรมของพื้นที่ภายใน",
+            message: "ISO Risk Evaluation Software",
+        }
+    });
+}
+const workevalCreate5 = (req, res, next) => {
+    res.render("work-eval-create-5", {
+        data: {
+            pageName: "ความเสี่ยงในกิจกรรมของพื้นที่ภายนอก",
+            message: "ISO Risk Evaluation Software",
+        }
+    });
+}
+const workevalCreate6 = (req, res, next) => {
+    res.render("work-eval-create-6", {
+        data: {
+            pageName: "ประเมินความเสี่ยงด้านสิ่งแวดล้อมในพื้นที่ของพื้นที่ภายใน",
+            message: "ISO Risk Evaluation Software",
+        }
+    });
+}
+const workevalCreate7 = (req, res, next) => {
+    res.render("work-eval-create-7", {
+        data: {
+            pageName: "ประเมินความเสี่ยงด้านความปลอดภัยในพื้นที่ของพื้นที่ภายใน",
+            message: "ISO Risk Evaluation Software",
+        }
+    });
+}
+const workevalCreate8 = (req, res, next) => {
+    res.render("work-eval-create-8", {
+        data: {
+            pageName: "ประเมินความเสี่ยงด้านสิ่งแวดล้อมในพื้นที่ของพื้นที่ภายนอก",
+            message: "ISO Risk Evaluation Software",
+        }
+    });
+}
+const workevalCreate9 = (req, res, next) => {
+    res.render("work-eval-create-9", {
+        data: {
+            pageName: "ประเมินความเสี่ยงด้านความปลอดภัยในพื้นที่ของพื้นที่ภายนอก",
+            message: "ISO Risk Evaluation Software",
+        }
+    });
+}
+const workevalCreate10 = (req, res, next) => {
+    res.render("work-eval-create-10", {
+        data: {
+            pageName: "ประเมินความเสี่ยงด้านสิ่งแวดล้อมในกิจกรรมของพื้นที่ภายใน",
+            message: "ISO Risk Evaluation Software",
+        }
+    });
+}
+const workevalCreate11 = (req, res, next) => {
+    res.render("work-eval-create-11", {
+        data: {
+            pageName: "ประเมินความเสี่ยงด้านความปลอดภัยในกิจกรรมของพื้นที่ภายใน",
+            message: "ISO Risk Evaluation Software",
+        }
+    });
+}
+const workevalCreate12 = (req, res, next) => {
+    res.render("work-eval-create-12", {
+        data: {
+            pageName: "ประเมินความเสี่ยงด้านสิ่งแวดล้อมในกิจกรรมของพื้นที่ภายนอก",
+            message: "ISO Risk Evaluation Software",
+        }
+    });
+}
+const workevalCreate13 = (req, res, next) => {
+    res.render("work-eval-create-13", {
+        data: {
+            pageName: "ประเมินความเสี่ยงด้านความปลอดภัยในกิจกรรมของพื้นที่ภายนอก",
+            message: "ISO Risk Evaluation Software",
+        }
+    });
+}
 
 module.exports.workevaluation = workevaluation;
 module.exports.workevalCreate1 = workevalCreate1;
 module.exports.workevalCreate2 = workevalCreate2;
 module.exports.workevalCreate3 = workevalCreate3;
+module.exports.workevalCreate4 = workevalCreate4;
+module.exports.workevalCreate5 = workevalCreate5;
+module.exports.workevalCreate6 = workevalCreate6;
+module.exports.workevalCreate7 = workevalCreate7;
+module.exports.workevalCreate8 = workevalCreate8;
+module.exports.workevalCreate9 = workevalCreate9;
+module.exports.workevalCreate10 = workevalCreate10;
+module.exports.workevalCreate11 = workevalCreate11;
+module.exports.workevalCreate12 = workevalCreate12;
+module.exports.workevalCreate13 = workevalCreate13;
--- routes/workEval.js
+++ routes/workEval.js
@@ -5,6 +5,16 @@ router
     .get("/work/evaluation", workEvalController.workevaluation)
     .get("/work/evaluation/create/1", workEvalController.workevalCreate1)
     .get("/work/evaluation/create/2", workEvalController.workevalCreate2)
-    .get("/work/evaluation/create/3", workEvalController.workevalCreate3);
+    .get("/work/evaluation/create/3", workEvalController.workevalCreate3)
+    .get("/work/evaluation/create/4", workEvalController.workevalCreate4)
+    .get("/work/evaluation/create/5", workEvalController.workevalCreate5)
+    .get("/work/evaluation/create/6", workEvalController.workevalCreate6)
+    .get("/work/evaluation/create/7", workEvalController.workevalCreate7)
+    .get("/work/evaluation/create/8", workEvalController.workevalCreate8)
+    .get("/work/evaluation/create/9", workEvalController.workevalCreate9)
+    .get("/work/evaluation/create/10", workEvalController.workevalCreate10)
+    .get("/work/evaluation/create/11", workEvalController.workevalCreate11)
+    .get("/work/evaluation/create/12", workEvalController.workevalCreate12)
+    .get("/work/evaluation/create/13", workEvalController.workevalCreate13);
 
 module.exports = router;
